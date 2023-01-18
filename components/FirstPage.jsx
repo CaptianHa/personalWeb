9dae8df45664f78e2d76dacee7fdd4532ad6d225
CaptianHa <97589878+CaptianHa@users.noreply.github.com>
2023-01-18 13:23:17 -0800

    1/18/23

--- components/FirstPage.jsx
+++ components/FirstPage.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import headshot from '../images/headShot.jpg'
+
+
 export default function FirstPage(){
     return(
         <div className="entire--page">
             <div className="first--page">
                 <h1 className="header">
-                    Hi, I'm Hayato.<br></br>I'm Majoring in <em>Computer Engineer.</em>
+                    Hi, I'm <em>Hayato.</em><br></br>I'm Majoring in <em>Computer Engineer.</em>
                 </h1>
-                <p1 className="first--about">
+                <p1 className="about">
                     Currently a <strong>junior</strong>, attending Gonzaga University
                     <br></br>
                     A <em>developer, explorer, and problem-solver</em>
@@ -19,9 +21,10 @@ export default function FirstPage(){
                 <hr className="line-1"></hr>
             </div>
             <div className="second--page">
-                <h2 className="header--second">About Me</h2>
+                <h2 className="header">About Me.</h2>
                 <div className="second--main">        
-                    <div className="about--me"> 
+                    <div className="about"> 
+                        <br></br>
                         Born and raised on the island of Guam  
                         <br></br>
                         Grew up playing many different sports and always being outside
@@ -34,12 +37,10 @@ export default function FirstPage(){
                     </div>
                     <br></br>
                     <img className = "headShot"src={headshot} />
-                </div>
-                
-              
+                </div>             
                 <div className="info">
                     
-                        <div className="info--detailed"> <strong><u>Experience</u></strong><br></br>3 years</div>
+                        <div className="info--detailed"><strong><u>Experience</u></strong><br></br>3 years</div>
                         <div className="info--detailed"><strong><u>Nationality</u></strong><br></br>Japanese</div>
                         <div className="info--detailed"> <strong><u>Locations</u></strong><br></br>Guam, USA<br></br>Washington, USA</div>
                         <div className="info--detailed"><strong><u>Reading</u></strong><br></br>The Power of Habit<br></br>by Charles Duhigg</div>
@@ -48,8 +49,11 @@ export default function FirstPage(){
                     
                     
                 </div>
+                <hr className="line-1"></hr>
             </div>
-
+            <div className="my--work">
+                <h1 className="header">My work.</h1>
+            </div>
         </div>
         
     )
